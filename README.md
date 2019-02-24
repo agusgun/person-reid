ff5da28268254a5e6ae83f672423acaa9ce1d0d5
agusgun <agusgun@protonmail.com>
2019-02-24 13:47:55 +0700

    Migrate detection code to new library for faster detection

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Source
 ## Detection
+
 ### Install Darknet
 Install YOLOv3 Darknet using this [link](https://github.com/AlexeyAB/darknet)
+
 ### Change Makefile
 Change the Makefile: `GPU=1`, `CUDNN=1`, `OPENCV=1`[Optional], `OPENMP=1`, and `LIBSO=1`
+
 ### Create symlink
 Create symlink using this command:  
 `sudo ln -s <DARKNET HOME DIRECTORY WITH .SO FILE> /usr/lib/x86_64-linux-gnu/libdarknet.so`
+
 ### Install Wrapper
 1. Clone the wrapper:  `git clone https://github.com/madhawav/YOLO3-4-Py` 
 2. Export CUDA_HOME path: `EXPORT CUDA_HOME=<CUDA_HOME_PATH>`
--- src/main/python/app/thread/detection_tracking_thread.py
+++ src/main/python/app/thread/detection_tracking_thread.py
@@ -3,31 +3,33 @@ from app.detection_tracking.detection import Detection
 from app.detection_tracking.tracking import Tracking
 import cv2 as cv
 import os
+from pydarknet import Image
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QThread
 from PyQt5.QtGui import QImage
+import time
 
 def bb_intersection_over_union(boxA, boxB):
-	# determine the (x, y)-coordinates of the intersection rectangle
-	xA = max(boxA[0], boxB[0])
-	yA = max(boxA[1], boxB[1])
-	xB = min(boxA[2], boxB[2])
-	yB = min(boxA[3], boxB[3])
- 
-	# compute the area of intersection rectangle
-	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
- 
-	# compute the area of both the prediction and ground-truth
-	# rectangles
-	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
- 
-	# compute the intersection over union by taking the intersection
-	# area and dividing it by the sum of prediction + ground-truth
-	# areas - the interesection area
-	iou = interArea / float(boxAArea + boxBArea - interArea)
- 
-	# return the intersection over union value
-	return iou
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
 
 class DetectionTrackingThread(QThread):
     changePixmap = pyqtSignal(QImage)
@@ -44,40 +46,49 @@ class DetectionTrackingThread(QThread):
         while True and self.is_running:
             ret, frame = self.input_thread.get_capture()
             if ret:
+                start_time = time.time()
+
                 # Detection Stuff Here
-                # Create a 4D blob from a frame.
-                blob = cv.dnn.blobFromImage(frame, 1/255, (self.detection.inpWidth, self.detection.inpHeight), [0,0,0], 1, crop=False)
-                # Sets the input to the network
-                self.detection.net.setInput(blob)
-                # Runs the forward pass to get output of the output layers
-                outs = self.detection.net.forward(self.detection.getOutputsNames(self.detection.net))
+                dark_frame = Image(frame)
+                outs = self.detection.net.detect(dark_frame, thresh=self.detection.confThreshold, nms=self.detection.nmsThreshold)
+                del dark_frame
+                
                 # Remove the bounding boxes with low confidence
                 boxes_for_tracking = self.detection.postprocess(frame, outs)
                 features = self.tracking.encoder(frame, boxes_for_tracking)
                 detections = [ddet(bbox, 1.0, feature) for bbox, feature in zip(boxes_for_tracking, features)]
 
-                self.tracking.tracker.predict()
-                self.tracking.tracker.update(detections)
+                # self.tracking.tracker.predict()
+                # self.tracking.tracker.update(detections)
 
-                for track in self.tracking.tracker.tracks:
-                    if not track.is_confirmed() or track.time_since_update > 1:
-                        continue
+                # for track in self.tracking.tracker.tracks:
+                #     if not track.is_confirmed() or track.time_since_update > 1:
+                #         continue
                     
-                    for idx_det, det in enumerate(detections):
-                        bbox_detection = det.to_tlbr()
-                        bbox_tracking = track.to_tlbr()
+                #     for idx_det, det in enumerate(detections):
+                #         bbox_detection = det.to_tlbr()
+                #         bbox_tracking = track.to_tlbr()
 
-                        if bb_intersection_over_union(bbox_detection, bbox_tracking) > 0.5:
-                            del detections[idx_det]
+                #         if bb_intersection_over_union(bbox_detection, bbox_tracking) > 0.5:
+                #             del detections[idx_det]
+                            
+                #             # if track.track_id in self.person_iterator_dict:
+                #             #     self.person_iterator_dict[track.track_id] += 1
+                #             # else:
+                #             #     self.person_iterator_dict[track.track_id] = 0
+                            
+                #             # crop_img = frame[int(bbox_tracking[1]): int(bbox_tracking[3]), int(bbox_tracking[0]): int(bbox_tracking[2])]
+                #             # dir_name = os.path.abspath(os.path.dirname(__file__))
+                #             # frame_output_filename = '../frame_output/' + str(track.track_id) + '_' + str(self.person_iterator_dict[track.track_id]) + '.png'
+                #             # cv.imwrite(os.path.join(dir_name, frame_output_filename), crop_img)
 
-                            cv.rectangle(frame, (int(bbox_detection[0]), int(bbox_detection[1])), (int(bbox_detection[2]), int(bbox_detection[3])), (255,255,255), 2)
-                            cv.putText(frame, str(track.track_id),(int(bbox_detection[0]), int(bbox_detection[1])), 0, 5e-3 * 200, (0,255,0), 2)
+                #             cv.rectangle(frame, (int(bbox_detection[0]), int(bbox_detection[1])), (int(bbox_detection[2]), int(bbox_detection[3])), (255,255,255), 2)
+                #             cv.putText(frame, str(track.track_id),(int(bbox_detection[0]), int(bbox_detection[1])), 0, 5e-3 * 200, (0,255,0), 2)
 
+                end_time = time.time()
                 # Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
-                t, _ = self.detection.net.getPerfProfile()
-                label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
+                label = 'Inference time: %.2f ms' % ((end_time - start_time)*1000)
                 cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255))
-                print(label)
                 # Convert frame to display to PyQt
                 rgbImage = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
                 convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
