9e2e7971affbb0504f99d4f7f2878e98ad431048
agusgun <agusgun@protonmail.com>
2019-04-09 22:07:08 +0700

    [Sys] Change keyframe extraction method

--- src/main/python/app/keyframe/face_keyframe.py
+++ src/main/python/app/keyframe/face_keyframe.py
@@ -4,102 +4,81 @@ from imutils import face_utils
 import numpy as np
 import os
 from scipy.spatial import distance
+from .face_detector import FaceDetector
+import matplotlib.pyplot as plt
 
 class FaceKeyframe: 
     def __init__(self):
-        curr_dir_path = os.path.dirname(__file__)
-        model_dir_path = os.path.join(curr_dir_path, '../model_data/')
-        cascade_classifier_path = os.path.join(model_dir_path + 'haarcascade_frontalface_alt.xml')
-        facial_landmark_extractor_path = os.path.join(model_dir_path + 'shape_predictor_68_face_landmarks.dat')
-        
-        self.cascade_classifier = cv.CascadeClassifier(cascade_classifier_path)
-        self.facial_landmark_extractor = dlib.shape_predictor(facial_landmark_extractor_path)
+        self.face_detector = FaceDetector(detector='mtcnn')
 
-        self.FACIAL_LANDMARKS_CORNERS_IDXS = {
-            'jaw': (0, 16),
-            'right_eyebrow': (17, 21),
-            'left_eyebrow': (22, 26),
-            'nose': (27, 33), # From Top to Bot
-            'right_eye': (36, 39), 
-            'left_eye': (42, 45),
-            'mouth': (48, 54)
-        }
+    def _normalize_keypoints(self, point, x, y):
+        return (point[0] - x, point[1] - y)
+    
+    def face_keyframe_check(self, filename, lower_threshold, higher_threshold):
+        img = cv.imread(filename)
+        if img is not None:
+            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+            face_and_landmark = self.face_detector.detect_and_extract_landmark(img)
+            if face_and_landmark is not None:
+                x, y, w, h = face_and_landmark['box']
+                if x < 0:
+                    x = 0
+                if y < 0:
+                    y = 0        
+                cropped_img = img[y:y+h, x:x+w]
+                
+                keypoints = face_and_landmark['keypoints']
+                # Normalize position
+                le_pos = self._normalize_keypoints(keypoints['left_eye'], x, y) 
+                re_pos =  self._normalize_keypoints(keypoints['right_eye'], x, y)
+                nose_pos =  self._normalize_keypoints(keypoints['nose'], x, y)
+                ml_pos =  self._normalize_keypoints(keypoints['mouth_left'], x, y)
+                mr_pos =  self._normalize_keypoints(keypoints['mouth_right'], x, y)
 
-    def detect_faces(self, gray_img, scale_factor=1.1, min_neighbor=5):
-        face_bboxes_list = self.cascade_classifier.detectMultiScale(gray_img, scaleFactor=scale_factor, minNeighbors=min_neighbor)
-        return face_bboxes_list
-
-    def facial_landmark_extraction(self, gray_img, face_bbox_position):
-        x, y, w, h = face_bbox_position
-
-        bbox_rectangle = dlib.rectangle(x, y, x+w, y+h)
-
-        facial_landmark_position = self.facial_landmark_extractor(gray_img, bbox_rectangle)
-        facial_landmark_position = face_utils.shape_to_np(facial_landmark_position)
-
-        return facial_landmark_position
-
-    def landmark_size_extraction(self, facial_landmark_position):
-        landmark = facial_landmark_position 
-        jaw_idx_corner1, jaw_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['jaw']
-        jaw_size = distance.euclidean(landmark[jaw_idx_corner1], landmark[jaw_idx_corner2])
-
-        right_eyebrow_idx_corner1, right_eyebrow_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['right_eyebrow']
-        right_eyebrow_size = distance.euclidean(landmark[right_eyebrow_idx_corner1], landmark[right_eyebrow_idx_corner2])
-
-        left_eyebrow_idx_corner1, left_eyebrow_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['left_eyebrow']
-        left_eyebrow_size = distance.euclidean(landmark[left_eyebrow_idx_corner1], landmark[left_eyebrow_idx_corner2])
-
-        nose_idx_corner1, nose_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['nose']
-        nose_size = distance.euclidean(landmark[nose_idx_corner1], landmark[nose_idx_corner2])
-
-        right_eye_idx_corner1, right_eye_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['right_eye']
-        right_eye_size = distance.euclidean(landmark[right_eye_idx_corner1], landmark[right_eye_idx_corner2])
-
-        left_eye_idx_corner1, left_eye_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['left_eye']
-        left_eye_size = distance.euclidean(landmark[left_eye_idx_corner1], landmark[left_eye_idx_corner2])
-
-        mouth_idx_corner1, mouth_idx_corner2 = self.FACIAL_LANDMARKS_CORNERS_IDXS['mouth']
-        mouth_size = distance.euclidean(landmark[mouth_idx_corner1], landmark[mouth_idx_corner2])
-
-        return (jaw_size, right_eyebrow_size, left_eyebrow_size, nose_size, right_eye_size, left_eye_size, mouth_size)
-
-    def face_keyframe_check(self, filename):
-        gray_img = cv.imread(filename, cv.IMREAD_GRAYSCALE)
-
-        bbox_of_faces = self.detect_faces(gray_img)
-        for x, y, w, h in bbox_of_faces:
-            face_img = gray_img[y:y+h, x:x+w]
-            face_resized_dimension = (60, 60)
-            face_img = cv.resize(face_img, face_resized_dimension, cv.INTER_AREA)
-
-            facial_landmark_position = self.facial_landmark_extraction(face_img, (0, 0, face_resized_dimension[0], face_resized_dimension[1]))
-
-            landmark_size = self.landmark_size_extraction(facial_landmark_position)
-            if landmark_size != None:
-                jaw_size, reb_size, leb_size, nose_size, re_size, le_size, mouth_size = landmark_size
-
-                # Edit the condition here
-                if (jaw_size > 50 and nose_size > 15):
-                    return face_img
+                dX = re_pos[0] - le_pos[0]
+                dY = re_pos[1] - le_pos[1]
+                angle = np.degrees(np.arctan2(dY, dX))
+                if angle >= lower_threshold and angle <= higher_threshold:
+                    return (cropped_img, angle)
+                else:
+                    return None
             else:
                 return None
-
+        else:
+            return None
+        
     def generate_keyframes_from_frames(self, person_id, input_dir_path, output_dir_path):
-        input_path_list = os.listdir(input_dir_path)
-        input_path_list = [os.path.join(input_dir_path, file_path) for file_path in input_path_list]
-        condition = '/' + str(person_id) + '_'
-        input_path_list = [file_path for file_path in input_path_list if condition in file_path]
-
+        person_input_dir_path = os.path.join(input_dir_path, str(person_id))
+        file_paths = os.listdir(person_input_dir_path)
+        file_paths = [os.path.join(person_input_dir_path, file_path) for file_path in file_paths]
         counter = 0
         print("generate keyframe from keyframe_id = ", person_id)
-        for file_path in input_path_list:
-            face_img = self.face_keyframe_check(file_path)
-            if not face_img is None:
-                keyframe_output_path = 'K' + str(person_id) + '_' + str(counter) + '.png'
-                keyframe_output_path = os.path.join(output_dir_path, keyframe_output_path)
-                cv.imwrite(keyframe_output_path, face_img)
-                counter += 1
-            
-            if counter == 10:
+        MAX = 100
+
+        face_images = []
+        LOWER_THRESHOLD = -5
+        HIGHER_THRESHOLD = 5
+        for file_path in file_paths[:MAX]:
+            face_img = self.face_keyframe_check(file_path, LOWER_THRESHOLD, HIGHER_THRESHOLD)
+            if face_img is not None:
+                face_images.append(face_img)
+        
+        keyframe_output_dir_path = os.path.join(output_dir_path, str(person_id))
+        if not os.path.exists(keyframe_output_dir_path):
+            os.makedirs(keyframe_output_dir_path)
+
+        face_images = sorted(face_images, key=lambda x: x[1])
+        increment = 1
+        NUMBER_OF_KEYFRAME = 10
+        if len(face_images) > NUMBER_OF_KEYFRAME:
+            increment = len(face_images) // NUMBER_OF_KEYFRAME
+        
+        counter = 0
+        for face_img in face_images[::increment]:
+            img, angle = face_img
+            keyframe_output_path = os.path.join(keyframe_output_dir_path, str(counter) + '.png')
+            img = cv.cvtColor(img, cv.COLOR_RGB2BGR)
+            cv.imwrite(keyframe_output_path, img)
+            counter += 1
+            if counter == NUMBER_OF_KEYFRAME:
                 break
