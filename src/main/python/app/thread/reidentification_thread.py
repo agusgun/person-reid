861a95a46777929dbba87bf721fae439959f689c
agusgun <agusgun@protonmail.com>
2019-04-24 23:31:01 +0700

    [Sys] Add condition when no keyframe generated

--- src/main/python/app/thread/reidentification_thread.py
+++ src/main/python/app/thread/reidentification_thread.py
@@ -36,27 +36,28 @@ class ReidentificationThread(QThread):
                 
                 # Predict face keyframe
                 print('Reidentify', keyframe_id)
-                features = self.face_reidentification.extract_feature_from_keyframes(self.face_keyframe_output_dir_path, keyframe_id)
-                prediction_matches = self.face_reidentification.predict_and_find_match_batch(self.image_representation_database, self.image_representation_paths, self.image_representation_label, features, self.THRESHOLD)
-                current_face_keyframe_dir_path = os.path.join(self.face_keyframe_output_dir_path, str(keyframe_id))
-                image_paths = [os.path.join(current_face_keyframe_dir_path, path) for path in os.listdir(current_face_keyframe_dir_path)]
-                
-                majority = self._find_majority_prediction_match(prediction_matches)
-                print('Majority', majority)
-                if majority[1] != 0: # no keyframe found
-                    if majority[0] == None: # new person
-                        print('New Person')
-                        self.counter += 1
-                        self.image_representation_database.append(features)
-                        self.image_representation_label.append(self.counter)
-                        self.image_representation_paths.append(image_paths)
-                        self.update_display_trigger.emit(self.counter, keyframe_id, [prediction[1] for prediction in prediction_matches])
-                    else: # existing person
-                        print('Existing Person', majority[0])
-                        self.image_representation_database.append(features)
-                        self.image_representation_label.append(majority[0])
-                        self.image_representation_paths.append(image_paths)
-                        self.update_display_trigger.emit(majority[0], keyframe_id, [prediction[1] for prediction in prediction_matches])
+                if os.path.exists(os.path.join(self.face_keyframe_output_dir_path, str(keyframe_id))): # to handle no keyframe generated because of no frame generated
+                    features = self.face_reidentification.extract_feature_from_keyframes(self.face_keyframe_output_dir_path, keyframe_id)
+                    prediction_matches = self.face_reidentification.predict_and_find_match_batch(self.image_representation_database, self.image_representation_paths, self.image_representation_label, features, self.THRESHOLD)
+                    current_face_keyframe_dir_path = os.path.join(self.face_keyframe_output_dir_path, str(keyframe_id))
+                    image_paths = [os.path.join(current_face_keyframe_dir_path, path) for path in os.listdir(current_face_keyframe_dir_path)]
+                    
+                    majority = self._find_majority_prediction_match(prediction_matches)
+                    print('Majority', majority)
+                    if majority[1] != 0: # no keyframe found
+                        if majority[0] == None: # new person
+                            print('New Person')
+                            self.counter += 1
+                            self.image_representation_database.append(features)
+                            self.image_representation_label.append(self.counter)
+                            self.image_representation_paths.append(image_paths)
+                            self.update_display_trigger.emit(self.counter, keyframe_id, [prediction[1] for prediction in prediction_matches])
+                        else: # existing person
+                            print('Existing Person', majority[0])
+                            self.image_representation_database.append(features)
+                            self.image_representation_label.append(majority[0])
+                            self.image_representation_paths.append(image_paths)
+                            self.update_display_trigger.emit(majority[0], keyframe_id, [prediction[1] for prediction in prediction_matches])
 
     def _find_majority_prediction(self, predictions):
         map = {}
