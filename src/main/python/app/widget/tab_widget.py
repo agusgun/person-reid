9b5877c6e2767256bcf528b72eb55b8bdd7bc262
agusgun <agusgun@protonmail.com>
2019-05-13 00:20:14 +0700

    [Sys] Add condition handler in tab widget

--- src/main/python/app/widget/tab_widget.py
+++ src/main/python/app/widget/tab_widget.py
@@ -1,24 +1,30 @@
 from .tabs.detection_tracking_tab import DetectionTrackingTab
+from .tabs.detection_reidentification_tab import DetectionReidentificationTab
 from .tabs.input_tab import InputTab
 from .tabs.reidentification_tab import ReidentificationTab
 from PyQt5.QtWidgets import QTabWidget, QVBoxLayout, QWidget
 
+import os
 class TabWidget(QWidget):
     def __init__(self, parent=None):
         super(TabWidget, self).__init__(parent)
 
         self.tabs = QTabWidget()
         self.input_tab = InputTab()
-        self.detection_tracking_tab = DetectionTrackingTab(self.input_tab.input_widget.th_input)
-        self.reid_tab = ReidentificationTab()
+        
         self.tabs.addTab(self.input_tab, "Input")
-        self.tabs.addTab(self.detection_tracking_tab, "Detection and Tracking")
-        self.tabs.addTab(self.reid_tab, "Re-identification")
+        if bool(os.environ['PERSON_REID_DIRECT_REIDENTIFICATION']):
+            self.detection_reidentification_tab = DetectionReidentificationTab(self.input_tab.input_widget.th_input)
+            self.tabs.addTab(self.detection_reidentification_tab, "Detection and Re-identification")
+        else:
+            self.detection_tracking_tab = DetectionTrackingTab(self.input_tab.input_widget.th_input)
+            self.reid_tab = ReidentificationTab()
+            self.tabs.addTab(self.detection_tracking_tab, "Detection and Tracking")
+            self.tabs.addTab(self.reid_tab, "Re-identification")
+            self.detection_tracking_tab.th_detection_tracking.change_person_id.connect(
+            self.reid_tab.th_reidentification.set_person_id)
 
         layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.addWidget(self.tabs)
         self.setLayout(layout)
-
-        self.detection_tracking_tab.th_detection_tracking.change_person_id.connect(
-            self.reid_tab.th_reidentification.set_person_id)
